94efef12632d2d97e55dd2ae976d227ee7a35822
ksjadeja <kuldeepjadeja459@gmail.com>
2019-10-05 08:37:12 +0530

    controller updated

--- src/com/controller/Controller.java
+++ src/com/controller/Controller.java
@@ -3,8 +3,11 @@ package com.controller;
 import com.Dao.ConnectionClass;
 import com.bean.CompanyBean;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -12,6 +15,17 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Controller {
+    public ChoiceBox<String> searchChoiceBox;
+    public CheckBox education;
+    public CheckBox environment;
+    public CheckBox socialServices;
+    public CheckBox healthcare;
+    public CheckBox innovation;
+    public CheckBox research;
+    public CheckBox renewableEnergy;
+    public CheckBox ruralDevelopment;
+    public Button submitCategories;
+
     @FXML
     ChoiceBox<String> choiceBox;
     @FXML
@@ -48,4 +62,117 @@ public class Controller {
 
         }
     }
-}
+
+    public void submitButtonCategories(MouseEvent mouseEvent) {
+        String type = searchChoiceBox.getValue();
+        String tableName="";
+        if(type.equals("NGO")) {
+            tableName = "ngos";
+        }
+        else {
+            tableName = "products";
+        }
+            Connection con = ConnectionClass.getConnection();
+
+                int cnt=0;
+                String str="select * from "+tableName+" where ";
+                   if(research.isSelected())
+                   {
+                       str = str.concat("research=1");
+                   }
+                   if(education.isSelected())
+                   {
+                       str = str.concat("or education=1");
+                   }
+                   if(environment.isSelected())
+                   {
+                       str = str.concat("or environment=1");
+                   }
+                    if(healthcare.isSelected())
+                    {
+                        str = str.concat("or healthcare=1");
+                    }
+                    if(innovation.isSelected())
+                    {
+                        str = str.concat("or innovation=1");
+                    }
+                    if(socialServices.isSelected())
+                    {
+                        str = str.concat("or socialServices=1");
+                    }
+                    if(renewableEnergy.isSelected())
+                    {
+                        str = str.concat("or renewableEnergy=1");
+                    }
+                    if(ruralDevelopment.isSelected())
+                    {
+                        str = str.concat("or ruralDevelopment=1");
+                    }
+                    System.out.println(str);
+
+            Connection connection = ConnectionClass.getConnection();
+            try {
+                Statement statement = con.createStatement();
+                statement.executeQuery(str);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+    }
+//    public void submitButtonCategories()
+//    {
+//        String type = searchChoiceBox.getValue();
+//        String tableName;
+//        if(type.equals("NGO")) {
+//            tableName = "ngo";
+//        }
+//        else {
+//            tableName = "products";
+//        }
+//            Connection con = ConnectionClass.getConnection();
+//
+//                int cnt=0;
+//                String str="select * from "+tableName+" where ";
+//                   if(research.isSelected())
+//                   {
+//                       str = str.concat("research=1");
+//                   }
+//                   if(education.isSelected())
+//                   {
+//                       str = str.concat("or education=1");
+//                   }
+//                   if(environment.isSelected())
+//                   {
+//                       str = str.concat("or environment=1");
+//                   }
+//                    if(healthcare.isSelected())
+//                    {
+//                        str = str.concat("or healthcare=1");
+//                    }
+//                    if(innovation.isSelected())
+//                    {
+//                        str = str.concat("or innovation=1");
+//                    }
+//                    if(socialServices.isSelected())
+//                    {
+//                        str = str.concat("or socialServices=1");
+//                    }
+//                    if(renewableEnergy.isSelected())
+//                    {
+//                        str = str.concat("or renewableEnergy=1");
+//                    }
+//                    if(ruralDevelopment.isSelected())
+//                    {
+//                        str = str.concat("or ruralDevelopment=1");
+//                    }
+//                    System.out.println(str);
+//
+//            Connection connection = ConnectionClass.getConnection();
+//            try {
+//                Statement statement = con.createStatement();
+//                statement.executeQuery(str);
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//        }
+    }
+
